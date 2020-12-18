ca989592d73d4dbf133c32f59982311dc73da754
nishth-code <75840100+nishth-code@users.noreply.github.com>
2020-12-18 18:05:29 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,32 @@
+var fixedRect,movingRect;
+
+
+function setup() {
+  createCanvas(800,800);
+
+  fixedRect = createSprite(600,400,50,80);
+  fixedRect.shapeColor = "green";
+  movingRect = createSprite(400,200,80,30);
+  movingRect.shapeColor = "green";
+  movingRect.velocityY = -5;
+  fixedRect.velocityY = +5;
+}
+
+function draw() {
+  background("black");
+  //movingRect.x = World.mouseX;
+ // movingRect.y = World.mouseY;
+  if(movingRect.x - fixedRect.x<fixedRect.width/2 + movingRect.width/2
+    && fixedRect.x - movingRect.x<fixedRect.width/2 + movingRect.width/2){
+    movingRect.velocityX = movingRect.velocityX*(-1);
+    fixedRect.velocityX = fixedRect.velocityX*(-1);
+    }
+    if(movingRect.y - fixedRect.y<fixedRect.width/2 + movingRect.width/2
+      && fixedRect.y - movingRect.y<fixedRect.width/2 + movingRect.width/2){
+      movingRect.velocityY = movingRect.velocityY*(-1);
+      fixedRect.velocityY = fixedRect.velocityY*(-1);
+      }
+  
+  console.log(movingRect.x - fixedRect.x);
+  drawSprites();
+}
